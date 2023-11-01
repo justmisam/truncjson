a407438c036ed6c703f84319e34b32206acd24e0
Misam <justmisam@gmail.com>
2023-11-01 23:06:05 +0100

    fix: return cp36

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
