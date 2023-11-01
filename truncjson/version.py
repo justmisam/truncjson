42c649a921a171211d96a68ef40a5addfbb7cc0a
Misam <justmisam@gmail.com>
2023-11-01 22:51:27 +0100

    fix: CIBW_BEFORE_BUILD

--- truncjson/version.py
+++ truncjson/version.py
@@ -1,4 +1,4 @@
 # Versions should comply with PEP440.
 # This line is parsed in setup.py:
-__version__ = '1.2.8'
+__version__ = '1.2.9'
 __version_info__ = tuple([int(i) for i in __version__.split('.')])
